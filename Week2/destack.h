65395de15562cea9ba2c12d1aa52d57213443be5
Almas-khan1 <125025360+Almas-khan1@users.noreply.github.com>
2025-02-21 23:29:51 +0500

    Update destack.h

---
+++ Week2/destack.h
@@ -0,0 +1,19 @@
+//#pragma once
+//template<class ItemType>
+//class destack {
+//
+//public:
+//	destack();
+//	destack(int max);
+//	~destack();
+//	int IsEmpty() const;
+//	int IsFull()const;
+//	void Push(ItemType newItem);
+//	void Pop(ItemType& item);
+//private:
+//	int top;
+//	int maxdestack;
+//	ItemType* items;
+//};
+//
+//
