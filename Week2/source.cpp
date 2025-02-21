8faa684dd45d4e474e7d7d28424a07336aea3449
Almas-khan1 <125025360+Almas-khan1@users.noreply.github.com>
2025-02-21 23:31:42 +0500

    Update source.cpp

---
+++ Week2/source.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//#include"stack.h"
+//#include"destack.h"
+//using namespace std;
+//int main()
+//{
+//	Stack s;
+//	int item;
+//	for (int i = 0; i < 20; i++)
+//		s.Push(i);
+//
+//	for (int i=0;i<20;i++)
+//	{
+//		s.Pop(item);
+//		cout << item << endl;
+//	}
+//
+//
+//	destack<int> intdestack;
+//	destack<float> floatdestack;
+//	int data;
+//	float val;
+//	intdestack.Push(35);
+//	floatdestack.Push(3.12);
+//	intdestack.Pop(data);
+//	cout << data << endl;
+//	floatdestack.Pop(val);
+//	cout << val << endl;
+//
+//	return 0;
+//}
